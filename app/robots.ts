bc9f571833d6ed50356dd230228ff6d067ada2ad
sihemzadi7-sys <sihemzadi7@gmail.com>
2026-06-27 15:48:38 +0100

    Refactor robots function to simplify rules structure

--- app/robots.ts
+++ app/robots.ts
@@ -1,23 +1,13 @@
 import type { MetadataRoute } from 'next'
 
 export default function robots(): MetadataRoute.Robots {
-  const baseUrl = 'https://www.ailiq.xyz'
-
   return {
-    rules: [
-      {
-        userAgent: '*',
-        allow: ['/', '/_next/static/', '/_next/image/'],
-        disallow: [
-          '/api/',
-          '/admin/',
-          '/dashboard/',
-          '/private/',
-          '/_next/data/',
-        ],
-      },
-    ],
-    sitemap: `${baseUrl}/sitemap.xml`,
-    host: baseUrl,
+    rules: {
+      userAgent: '*',
+      allow: '/',
+      disallow: ['/api/', '/admin/', '/dashboard/', '/private/', '/_next/data/'],
+    },
+    sitemap: 'https://www.ailiq.xyz/sitemap.xml',
+    host: 'https://www.ailiq.xyz',
   }
 }
