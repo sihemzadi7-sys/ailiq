224fb726f6d51de2b7616e47ec426aa2b98258cd
sihemzadi7-sys <sihemzadi7@gmail.com>
2026-06-28 21:16:25 +0100

    Create index.ts to aggregate various tool modules

---
+++ data/tools/index.ts
@@ -0,0 +1,23 @@
+import { writingTools } from './writing';
+import { imageTools } from './image';
+import { videoTools } from './video';
+import { codeTools } from './code';
+import { productivityTools } from './productivity';
+import { audioTools } from './audio';
+import { threeDTools } from './three-d';
+import { marketingTools } from './marketing';
+import { seoTools } from './seo';
+import { researchTools } from './research';
+
+export const tools = [
+  ...writingTools,
+  ...imageTools,
+  ...videoTools,
+  ...codeTools,
+  ...productivityTools,
+  ...audioTools,
+  ...threeDTools,
+  ...marketingTools,
+  ...seoTools,
+  ...researchTools,
+];
